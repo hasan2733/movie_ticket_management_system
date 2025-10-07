ab093f70f2469b900ea2f3c86f626fae36aec7f5
hasan2733 <hasan.tng33@gmail.com>
2025-10-07 21:35:15 +0600

    Initial commit by abid

---
+++ src/main/java/bd/edu/seu/ticket_booking/AdminController/ViewComplainController.java
@@ -0,0 +1,127 @@
+package bd.edu.seu.ticket_booking.AdminController;
+
+import bd.edu.seu.ticket_booking.DB.DBConnection;
+import bd.edu.seu.ticket_booking.HelloApplication;
+import bd.edu.seu.ticket_booking.Model.Complaint;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyEvent;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class ViewComplainController implements Initializable {
+
+    @FXML
+    private TableColumn<Complaint, String> commentsC;
+
+    @FXML
+    private TextArea datailsComments;
+
+    @FXML
+    private TableColumn<Complaint, String> nameC;
+
+    @FXML
+    private Label nameLabel;
+
+    @FXML
+    private TableView<Complaint> tableView;
+
+    @FXML
+    private TableColumn<Complaint, String> typeC;
+
+    @FXML
+    private Label typeOfComplainLabel;
+
+    @FXML
+    public TextField searchField;
+
+    ObservableList<Complaint> complaintsList = FXCollections.observableArrayList();
+
+    @FXML
+    public void backEvent(ActionEvent event) {
+        HelloApplication.changeScene("adminDashboard.fxml");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    nameC.setCellValueFactory(c->new SimpleStringProperty(c.getValue().getUsername()));
+    typeC.setCellValueFactory(c->new SimpleStringProperty(c.getValue().getComplaintType()));
+    commentsC.setCellValueFactory(c->new SimpleStringProperty(c.getValue().getMessage()));
+
+    loadComplaints();
+    tableView.getSelectionModel().selectedItemProperty().addListener((obs,old,val)->{
+        if(val!=null)
+        {
+            nameLabel.setText(val.getUsername());
+            typeOfComplainLabel.setText(val.getComplaintType());
+            datailsComments.setText(val.getMessage());
+            datailsComments.setDisable(true);
+        }
+    });
+    }
+
+    private void showAlert(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+    public void loadComplaints()
+    {
+        complaintsList.clear();
+        String sql = "select * from complaints";
+
+        try  {
+            Connection connection = DBConnection.getConnection();
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                complaintsList.add(new Complaint(
+                        rs.getInt("id"),
+                        rs.getString("username"),
+                        rs.getString("complaint_type"),
+                        rs.getString("message")
+                ));
+            }
+            tableView.setItems(complaintsList);
+
+            if (!complaintsList.isEmpty()) {
+                tableView.getSelectionModel().selectFirst();
+                Complaint first = tableView.getSelectionModel().getSelectedItem();
+                if (first != null) {
+                    nameLabel.setText(first.getUsername());
+                    typeOfComplainLabel.setText(first.getComplaintType());
+                    datailsComments.setText(first.getMessage());
+                    datailsComments.setDisable(true);
+                    nameLabel.setStyle("-fx-text-fill: red");
+                    typeOfComplainLabel.setStyle("-fx-text-fill: red");
+                }
+            }
+
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+            showAlert("Alert", "Database Error: " + e.getMessage());
+        }
+    }
+    @FXML
+    void searchEvent(KeyEvent event) {
+    String text = searchField.getText().toLowerCase();
+    List<Complaint> obs = complaintsList.stream().filter(c->c.getComplaintType().toLowerCase().startsWith(text)
+    || c.getUsername().toLowerCase().startsWith(text)|| c.getMessage().toLowerCase().startsWith(text)).toList();
+
+    tableView.setItems(FXCollections.observableArrayList(obs));
+    }
+}
---
+++ src/main/java/bd/edu/seu/ticket_booking/CustomerContoller/AddComplainController.java
@@ -0,0 +1,87 @@
+package bd.edu.seu.ticket_booking.CustomerContoller;
+
+import bd.edu.seu.ticket_booking.DB.DBConnection;
+import bd.edu.seu.ticket_booking.HelloApplication;
+import bd.edu.seu.ticket_booking.Utitlity.CurrentUser;
+import com.mysql.cj.xdevapi.Warning;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class AddComplainController implements Initializable {
+
+    @FXML
+    private ComboBox<String> complainComboBox;
+
+    @FXML
+    private TextArea textArea;
+
+    @FXML
+    private Label userLabel;
+
+    @FXML
+    public void addComplain(ActionEvent event) {
+        String type = complainComboBox.getValue();
+        String message = textArea.getText().trim();
+
+        if (type == null || type.isEmpty()) {
+            showAlert("Warnings", "Please select a complaint type.");
+            return;
+        }
+
+        if (message.isEmpty()) {
+            showAlert("Warnings", "Please write your complaint message.");
+            return;
+        }
+
+        String username = CurrentUser.getName();
+        String sql = "insert into complaints (username, complaint_type, message) values (?, ?, ?)";
+        try {
+            Connection connection = DBConnection.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, type);
+            preparedStatement.setString(3, message);
+            preparedStatement.executeUpdate();
+            showAlert("Success","Complain successfully submitted");
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void backEvent(ActionEvent event) {
+        HelloApplication.changeScene("customerDashboard.fxml");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ObservableList<String> obs = FXCollections.observableArrayList();
+        obs.add("System Related");
+        obs.add("Booking Related");
+        obs.add("Payment Related");
+        obs.add("Others");
+        complainComboBox.setItems(obs);
+        userLabel.setText(CurrentUser.getName());
+    }
+    private void showAlert(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+}
